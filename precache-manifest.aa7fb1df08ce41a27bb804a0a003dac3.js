8c153f891545c5b552dd0f3f454f59ffcf6c53e5
mohammed rizwan <mohammedrizwanta65@gmail.com>
2022-09-24 12:52:44 +0530

    Updates

---
+++ precache-manifest.aa7fb1df08ce41a27bb804a0a003dac3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec1d0b31fdf77e112e76",
+    "url": "/sample/static/js/main.ec1d0b31.chunk.js"
+  },
+  {
+    "revision": "48dc1c1c87d8faf3dbc4",
+    "url": "/sample/static/js/1.48dc1c1c.chunk.js"
+  },
+  {
+    "revision": "455bb57d6ec0ff8acc00",
+    "url": "/sample/static/js/runtime~main.455bb57d.js"
+  },
+  {
+    "revision": "9057b8773060fa537d7348853c15f83e",
+    "url": "/sample/index.html"
+  }
+];
